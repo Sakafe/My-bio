ca0e1315ea5024eafc6fe174afe46f0b3f5b65b0
sakafe <ahmedsakafe@gmail.com>
2023-03-15 17:51:20 +0600

    add new image

--- src/Component/Work/WorkData.js
+++ src/Component/Work/WorkData.js
@@ -1,7 +1,7 @@
 const projectData = [
     {
-        img:'https://cdn.wedevs.com/uploads/2017/10/website-project-management-wordpress.png',
-        title:'Wordpress development',
+        img:'https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/193491639/original/45150800145cc6b1490194d75388517c52578c42/be-your-front-end-developer-of-reactjs.png',
+        title:'Front-End Developer',
         text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto repellat aliquid dolorum impedit a fuga, quibusdam est animi, maiores id temporibus. Dolorum excepturi eos voluptates dicta. Assumenda in, repellat nihil accusamus nisi esse eaque atque reprehenderit debitis saepe ea obcaecati deleniti adipisci tempore officia corrupti quisquam nesciunt enim! A, odit.",
         
         view:'https://pilotselite.com/'
